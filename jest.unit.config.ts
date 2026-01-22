7f32e9f881f963baded34f3d2ac65057d050a0f2
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-22 21:52:19 +0100

    feat(jest): add unit test configuration

---
+++ jest.unit.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+import baseConfig from './jest.config';
+
+const config: Config = {
+  ...baseConfig,
+  displayName: 'unit',
+  testMatch: ['**/src/**/*.test.ts?(x)'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '\\.integration\\.test\\.tsx?$',
+  ],
+};
+
+export default config;

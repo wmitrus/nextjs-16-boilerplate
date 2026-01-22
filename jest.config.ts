9444ba473dd6c5b78fd8968f7d99a7569caacf9a
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-22 23:54:10 +0100

    feat(jest): enable global variable injection in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
+  injectGlobals: true,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^(\\.{1,2}/.*)\\.js$': '$1',

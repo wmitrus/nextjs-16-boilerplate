21bb34b11bccaa03da69439cc1a9bcdd41e717cc
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-23 00:21:28 +0100

    refactor(tests): switch from jest to vitest for mocking next/image
    - Replace jest.mock with vi.mock for compatibility with vitest
    - Update MockImage function to maintain existing functionality

--- tests/setup.tsx
+++ tests/setup.tsx
@@ -1,17 +1,19 @@
 import '@testing-library/jest-dom';
-import { jest } from '@jest/globals';
 import React from 'react';
+import { vi } from 'vitest';
 
 import { server } from '../src/shared/lib/mocks/server';
 
 // Mock next/image
-jest.mock('next/image', () => {
-  return function MockImage({
-    priority: _priority,
-    ...props
-  }: React.ImgHTMLAttributes<HTMLImageElement> & { priority?: boolean }) {
-    // eslint-disable-next-line @next/next/no-img-element
-    return <img {...props} alt={props.alt || ''} />;
+vi.mock('next/image', () => {
+  return {
+    default: function MockImage({
+      priority: _priority,
+      ...props
+    }: React.ImgHTMLAttributes<HTMLImageElement> & { priority?: boolean }) {
+      // eslint-disable-next-line @next/next/no-img-element
+      return <img {...props} alt={props.alt || ''} />;
+    },
   };
 });
 

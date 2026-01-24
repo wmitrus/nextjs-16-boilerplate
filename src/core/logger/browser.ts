c24d71aaccf119ff1a0aba7064865cc8fccd28c9
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-24 23:11:34 +0100

    feat(logger): add browser logger implementation with Pino

---
+++ src/core/logger/browser.ts
@@ -0,0 +1,21 @@
+import pino, { type Logger, type LoggerOptions } from 'pino';
+
+import { env } from '@/core/env';
+
+import { createLogflareBrowserTransport } from './utils';
+
+const options: LoggerOptions = {
+  level: env.NEXT_PUBLIC_LOG_LEVEL || 'info',
+  browser: {
+    asObject: true,
+    transmit: env.NEXT_PUBLIC_LOGFLARE_INTEGRATION_ENABLED
+      ? createLogflareBrowserTransport().transmit
+      : undefined,
+  },
+  base: {
+    env: process.env.VERCEL_ENV || process.env.NODE_ENV,
+    revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+  },
+};
+
+export const browserLogger: Logger = pino(options);

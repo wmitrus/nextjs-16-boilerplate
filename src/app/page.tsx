831f5d92d5cadc5bde9f5893c24af64ce866c9da
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-25 18:06:47 +0100

    feat(page): add BrowserLoggerProbe component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import BrowserLoggerProbe from './components/BrowserLoggerProbe';
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -33,6 +35,7 @@ export default function Home() {
             </a>{' '}
             center.
           </p>
+          <BrowserLoggerProbe />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a

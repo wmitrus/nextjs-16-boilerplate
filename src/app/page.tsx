b50ac2d1bc13398c7ce1d8d88d6640aecc241fca
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-25 20:38:19 +0100

    refactor(home): remove BrowserLoggerProbe component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 
-import BrowserLoggerProbe from './components/BrowserLoggerProbe';
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -35,7 +33,6 @@ export default function Home() {
             </a>{' '}
             center.
           </p>
-          <BrowserLoggerProbe />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a

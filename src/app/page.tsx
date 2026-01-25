ab870b67ac76d3b7716974aa3f902d1aee8f1f70
Wojciech Mitruś <wmitrus@gmail.com>
2026-01-25 21:01:59 +0100

    feat(logger): add logging for home page rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 
+import { logger } from '@/core/logger';
+
 export default function Home() {
+  logger.debug('Rendering home page');
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">

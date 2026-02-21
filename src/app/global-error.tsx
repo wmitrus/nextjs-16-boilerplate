a8650da91a5a2a26143755d9740c2cb1ed923247
Wojciech Mitruś <wmitrus@gmail.com>
2026-02-21 17:42:20 +0100

    refactor(error): enhance logger configuration in GlobalError component

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -2,7 +2,13 @@
 
 import { useEffect } from 'react';
 
-import { logger } from '@/core/logger/client';
+import { logger as baseLogger } from '@/core/logger/client';
+
+const logger = baseLogger.child({
+  type: 'UI',
+  category: 'error-boundary',
+  module: 'global-error',
+});
 
 export default function GlobalError({
   error,

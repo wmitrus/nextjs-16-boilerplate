29e57e43a55dbdf35af432f41508f339701fcb56
Wojciech Mitruś <wmitrus@gmail.com>
2026-02-26 00:15:39 +0100

    feat(playwright): add global setup configuration for tests

--- playwright.vscode.config.ts
+++ playwright.vscode.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  globalSetup: './e2e/global.setup.ts',
   reporter: 'line',
   use: {
     baseURL: 'http://localhost:3000',

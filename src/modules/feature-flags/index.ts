c5574bb44d1a8d1e14adce3aba7b50e300fd55b1
Wojciech Mitruś <wmitrus@gmail.com>
2026-02-28 15:55:37 +0100

    feat(feature-flags): add exports for InMemory and OpenFeature services

---
+++ src/modules/feature-flags/index.ts
@@ -0,0 +1,2 @@
+export { InMemoryFeatureFlagService } from './infrastructure/memory/InMemoryFeatureFlagService';
+export { OpenFeatureFeatureFlagService } from './infrastructure/openfeature/OpenFeatureFeatureFlagService';

9a75b3e193640b4df086ec6ccf46b1d5ef5aa846
Wojciech Mitruś <wmitrus@gmail.com>
2026-03-01 16:39:48 +0100

    refactor(db): add DbRuntime type to exports in index.ts

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -1,2 +1,8 @@
 export { createDb } from './create-db';
-export type { DrizzleDb, DbConfig, DbDriver, DbProvider } from './types';
+export type {
+  DrizzleDb,
+  DbConfig,
+  DbDriver,
+  DbProvider,
+  DbRuntime,
+} from './types';

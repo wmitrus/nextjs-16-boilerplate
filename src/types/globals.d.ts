98dd328a19ffc490f3aafeda8ea2a7bba7ff0980
Wojciech Mitruś <wmitrus@gmail.com>
2026-03-08 15:01:33 +0100

    feat(types): add email and primaryEmail to CustomJwtSessionClaims

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,6 +2,8 @@ export {};
 
 declare global {
   interface CustomJwtSessionClaims {
+    email?: string;
+    primaryEmail?: string;
     metadata: {
       onboardingComplete?: boolean;
       displayName?: string;

0e04c0112479281d50fabe32476d77418c353171
Wojciech Mitruś <wmitrus@gmail.com>
2026-03-18 23:18:12 +0100

    refactor(onboarding): simplify OnboardingPage component - remove OnboardingClientProbe from OnboardingPage - streamline rendering to only include OnboardingForm - improve code readability and maintainability

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,11 +1,5 @@
-import { OnboardingClientProbe } from './onboarding-client-probe';
 import { OnboardingForm } from './onboarding-form';
 
 export default function OnboardingPage() {
-  return (
-    <>
-      <OnboardingForm />
-      <OnboardingClientProbe />
-    </>
-  );
+  return <OnboardingForm />;
 }
